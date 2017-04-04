55caf4a83267fc221e0e5f571459fb9e12918622
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-04-03 17:05:25 -0700

    Program is ready for code, just need to write it all, haha

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -9,3 +9,17 @@ end
 #Takes an array of data, turns it into binary tree of Node objects, in appropriate order
 def build_tree(data)
 end
+
+#Searches the tree for a target value, returns the node at which value is located
+def breadth_first_search(value)
+end
+
+#Searches the tree for a target value, returns node at which value is located
+def depth_first_search(value)
+end
+
+#Recursive version of #depth_first_search
+def dfs_rec
+end
+
+build_tree([1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345])
