203a89886293439d17d2eba5b5d07e4b3eaff78d
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-04-13 17:24:45 -0700

    Began work again, init method works, #create_node has the same issue as before, can't access values of left and right nodes.

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,32 +1,48 @@
 class Node
-  attr_accessor :value, :next_node, :parent_node, :child_node
-  def initialize(value)
+  attr_accessor :value, :parent_node, :right_node, :left_node
+  def initialize(value = nil)
     @value = value
-    @next_node = nil
-    @child_node = nil
-    @parent_node = nil
   end
 end
+
+class Binary_Tree
 
 #Takes an array of data, turns it into binary tree of Node objects, in appropriate order
-def build_tree(data)
-  data.each_with_index do |value, index|
-    data[index] = Node.new(value)
+  def build_tree(data)
+    @root = Node.new(data[0])
+    puts @root.value
+    data.each do |element|
+      create_node(element)
+    end
   end
-  puts data
-  puts data[1].class
-end
+
+  def create_node(value)
+    node = Node.new(value)
+    if(node.value < @root.value) && (@root.left_node == nil)
+      @root.left_node = node
+    elsif(node.value >= @root.value) && (@root.right_node == nil)
+      @root.right_node = node
+    end
+    puts @root.left_node
+    puts @root.right_node
+  end
 
 #Searches the tree for a target value, returns the node at which value is located
-def breadth_first_search(value)
-end
+  def breadth_first_search(value)
+
+  end
 
 #Searches the tree for a target value, returns node at which value is located
-def depth_first_search(value)
-end
+  def depth_first_search(value)
+  end
 
 #Recursive version of #depth_first_search
-def dfs_rec
+  def dfs_rec
+  end
+
 end
 
-build_tree([1, 3, 4, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345])
+
+t = Binary_Tree.new
+t.build_tree([1, 0, 3])
+#, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345])
