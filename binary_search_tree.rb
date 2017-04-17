5059acdbea177df0032e6aa192e6e1886f195dee
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-04-17 16:39:49 -0700

    Wrote #BFS method, it doesn't work totally, but i think that's the fault of my #create_node method

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -3,6 +3,9 @@ class Node
   def initialize(value = nil)
     @value = value
   end
+  def value
+    return @value
+  end
 end
 
 class Binary_Tree
@@ -41,7 +44,19 @@ class Binary_Tree
 
 #Searches the tree for a target value, returns the node at which value is located
   def breadth_first_search(value)
-
+    queue = [@root]
+    queue.each do |node|
+      if(node.value == value)
+        puts node.value
+      end
+      if(node.left_node != nil)
+        queue << node.left_node
+      end
+      if(node.right_node != nil)
+        queue << node.right_node
+      end
+      return nil
+    end
   end
 
 #Searches the tree for a target value, returns node at which value is located
@@ -58,3 +73,4 @@ end
 t = Binary_Tree.new
 t.build_tree([2, 1, 3])
 #, 4, 5, 7, 7, 8, 9, 9, 23, 67, 324, 6345])
+t.breadth_first_search(1)
